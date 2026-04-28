5dd736d5718d3200b563071c88528ccb6d19e3cc
Medenchi <denlifik@gmail.com>
2026-04-28 19:59:35 +0300

    Update vite.config.ts

--- MalinaPreza/vite.config.ts
+++ MalinaPreza/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // поменяй на '/malinapreza/' если нужен subpath на GitHub Pages
+  base: '/MalinaPreza/',  // ← ВАЖНО! С большой буквы и со слешами
 })
